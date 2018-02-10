09bddf018cc68c5ed5d51997aaccbac852943541
jklughammer <jklughammer@cemm.oeaw.ac.at>
2018-02-10 22:46:44 +0100

    add SFRP2

--- src/02.4-meth_overview_tracks.R
+++ src/02.4-meth_overview_tracks.R
@@ -35,9 +35,9 @@ rm(rrbsCg)
 #plot tracks 
 
 #regions to plot
-POIs=list(enhancer51330=c("chr22", 50298971,50304771),enhancer88653=c("chr7", 151114913,151115513),enhancer84097=c("chr7", 1667764,1669964),enhancer49388=c("chr21", 45016885, 45020285),PLXNB2=c("chr22",50274644,50309283),BCL2L11=c("chr2",111113953,111169264),SFRP2=c("chr4",153780540,153794943),MGMT=c("chr10",129445717,129773284))
+POIs=list(enhancer51330=c("chr22", 50298971,50304771),enhancer88653=c("chr7", 151114913,151115513),enhancer84097=c("chr7", 1667764,1669964),enhancer49388=c("chr21", 45016885, 45020285),PLXNB2=c("chr22",50274644,50309283),BCL2L11=c("chr2",111113953,111169264),SFRP2=c("chr4",153780540,153794943),MGMT=c("chr10",129445717,129773284),TERT=c("chr5",1248767,1302098))
 
-#POIs=list()
+#POIs=list(TERT=c("chr5",1248767,1302098))
 
 #now plot
 for (POI in POIs){
@@ -46,10 +46,10 @@ for (POI in POIs){
   lower=as.numeric(POI[2])
   upper=as.numeric(POI[3])
   
-  sub=rrbsCg_annot[regions=="CG"&surgery.x%in%c(1,2)&category%in%c("GBMatch")&IDH=="wt"&chr==sel_chr&start>lower&end<upper]
+  sub=rrbsCg_annot[regions=="CG"&surgery.x%in%c(1,2)&category%in%c("GBMatch","GBmatch_val")&IDH=="wt"&chr==sel_chr&start>lower&end<upper]
   sub[,id:=factor(id,levels=unique(id[order(surgery.x)])),]
-  pdf(paste0("methylation_track_CG_",sel_chr,"_",lower,".pdf"),width=5,height=5)
-    pl=ggplot(sub)+geom_tile(width=0.005*(upper-lower), aes(fill=methyl,x=start,y=id))+xlab(paste0(sel_chr," (M)"))+scale_fill_gradient(low="blue",high="red")+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank())+theme(legend.position="bottom")+facet_wrap(~surgery.x,ncol=1,scale="free")+xlim(c(lower,upper))
+  pdf(paste0("methylation_track_CG_",sel_chr,"_",lower,".pdf"),width=5,height=7)
+    pl=ggplot(sub)+geom_tile(width=0.005*(upper-lower), aes(fill=methyl,x=start,y=id))+xlab(paste0(sel_chr," (M)"))+scale_fill_gradient(low="blue",high="red")+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank())+theme(legend.position="bottom")+facet_grid(paste0(category,"\n",surgery.x)~.,scales="free_y",space="free_y")+xlim(c(lower,upper))
     print(pl)
   dev.off()
 }
