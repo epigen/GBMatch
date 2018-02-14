f66c92d34187b58e8ceff044cf0cb2bb72e42a24
jklughammer <jklughammer@cemm.oeaw.ac.at>
2018-02-15 00:13:25 +0100

    add EPM and DPM

--- src/10.1_add_annotation.R
+++ src/10.1_add_annotation.R
@@ -20,6 +20,23 @@ setnames(CNV_chrom_arms,"sample","N_number_seq")
 loadCaches("combined_heterogeneity")
 setnames(combined_heterogeneity,"id","N_number_seq")
 
+#get EPM
+EPM_files=list.files(file.path(getOption("PROCESSED.PROJECT"),"results_analysis/06-methclone/"),"EPM_1vs2.tsv",recursive=TRUE,full.names=TRUE)
+
+rm(EPM_all)
+for (EPM_file in EPM_files){  
+  setup=gsub(".*summary_minReads|\\/EPM_epy_cutoff|\\/EPM_1vs2.tsv","",EPM_file)
+  EPM=fread(EPM_file,select=c("patient","EPM"))
+  setnames(EPM,names(EPM),c("patID",paste0("EPM_1vs2_",setup)))
+  
+  if (!exists("EPM_all")){EPM_all=EPM}else{
+  EPM_all=merge(EPM_all,EPM,by="patID",all=TRUE)
+  }
+}
+
+#get DPM
+DPM=fread(file.path(getOption("PROCESSED.PROJECT"),"results_analysis/11.2-diffMeth_single/DPM.tsv"),select=c("patID","DPM"))
+
 
 #get mutational load
 mut_load=fread(file.path(getOption("PROCESSED.PROJECT"),"results_analysis/04-bissnp/bissnp_var_samp.tsv"))
@@ -55,7 +72,7 @@ SFRP2_meth=fread(file.path(getOption("PROCESSED.PROJECT"),"results_analysis/11.2
 
 #column annotation
 load("column_annoation.RData")
-column_annotation_mol=list(CNV_chrom_arms=names(CNV_chrom_arms), meth_heterogeneity=names(combined_heterogeneity),mutational_load=names(mut_load),transc_subgroups=names(transc_subgroups),dipScores=names(dip_scores),mgmt_status=names(mgmt_status),SFRP2_meth=names(SFRP2_meth))
+column_annotation_mol=list(CNV_chrom_arms=names(CNV_chrom_arms), meth_heterogeneity=names(combined_heterogeneity),mutational_load=names(mut_load),transc_subgroups=names(transc_subgroups),dipScores=names(dip_scores),mgmt_status=names(mgmt_status),SFRP2_meth=names(SFRP2_meth),EPM=grep("patID",names(EPM_all),invert=TRUE,value=TRUE),DPM=grep("patID",names(DPM),invert=TRUE,value=TRUE))
 
 column_annotation_combined=c(column_annotation,column_annotation_mol)
 
@@ -66,21 +83,21 @@ column_annotation_combined_clean=lapply(column_annotation_combined,function(x){g
 #column categories
 technical=c(column_annotation_combined_clean$random_fragmentation,column_annotation_combined_clean$sequencing_stats,column_annotation_combined_clean$sequencing_annot,"Image width [pix]","Image height [pix]","Block size N","material")
 medical=c(column_annotation_combined_clean$psa,column_annotation_combined_clean$clinical_annotation,column_annotation_combined_clean$imaging,column_annotation_combined_clean$imaging_progression,"WHO2016_classification", "WHO2016_classification_comment")
-measurement=c(column_annotation_combined_clean$histo_classification,column_annotation_combined_clean$histo_segmentation,column_annotation_combined_clean$imaging_segmentation,column_annotation_combined_clean$CNV_chrom_arms,column_annotation_combined_clean$CNV_GOIs,column_annotation_combined_clean$meth_heterogeneity,column_annotation_combined_clean$mutational_load,column_annotation_combined_clean$transc_subgroups,column_annotation_combined_clean$dipScores,column_annotation_combined_clean$mgmt_status,column_annotation_combined_clean$SFRP2_meth)
+measurement=c(column_annotation_combined_clean$histo_classification,column_annotation_combined_clean$histo_segmentation,column_annotation_combined_clean$imaging_segmentation,column_annotation_combined_clean$CNV_chrom_arms,column_annotation_combined_clean$CNV_GOIs,column_annotation_combined_clean$meth_heterogeneity,column_annotation_combined_clean$mutational_load,column_annotation_combined_clean$transc_subgroups,column_annotation_combined_clean$dipScores,column_annotation_combined_clean$mgmt_status,column_annotation_combined_clean$SFRP2_meth,column_annotation_combined_clean$EPM,column_annotation_combined_clean$DPM)
 
 column_category=list(technical=technical,medical=medical[!medical%in%technical],measurement=measurement[!measurement%in%c(technical,medical)])
 
 
-
-
 #now merge
-merge_objects=c("annotation","CNV_chrom_arms","combined_heterogeneity","mut_load","transc_subgroups","dip_scores","mgmt_status","SFRP2_meth")
+merge_objects=c("annotation","CNV_chrom_arms","combined_heterogeneity","mut_load","transc_subgroups","dip_scores","mgmt_status","SFRP2_meth","EPM_all","DPM")
 
 combined_annotation=data.table()
 for(merge_object in merge_objects){
   print(merge_object)
   if(length(combined_annotation)==0){combined_annotation=get(merge_object)}else{
-    combined_annotation=merge(combined_annotation,get(merge_object),by="N_number_seq",all=TRUE)
+    if(merge_object%in%c("EPM_all","DPM")){
+      combined_annotation=merge(combined_annotation,get(merge_object),by="patID",all=TRUE)}else{
+      combined_annotation=merge(combined_annotation,get(merge_object),by="N_number_seq",all=TRUE)}
   }
 }
 
